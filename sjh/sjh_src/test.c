13fbe66c9905bdfa7ff174e221e9f1f5ca41e7da
ShinJaeHo <vlovev1011@gmail.com>
2016-11-10 21:13:47 -0800

    1

---
+++ sjh/sjh_src/test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void main()
+{
+	char game_screen[22][28];
+
+	for(int i=0; i<22; i++)
+        {
+                for(int z=0; z<28; z++)
+                {
+                        game_screen[i][z]=32;
+			//tan - 9
+                }
+        }
+
+	for(int i=0; i<22; i++)
+	{
+		game_screen[i][0]='*';
+		game_screen[i][27]='*';
+	}
+	for(int i=0; i<28; i++)
+	{
+		game_screen[0][i]='*';
+		game_screen[21][i]='*';
+	}
+	for(int i=0; i<22; i++)
+	{
+		for(int z=0; z<28; z++)
+		{
+			printf("%c",game_screen[i][z]);
+		}
+		printf("\n");
+	}
+}
+
