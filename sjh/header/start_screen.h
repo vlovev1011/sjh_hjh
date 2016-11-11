b7088ac4632d7b61292e64c9d1f7b95593f5d5be
ShinJaeHo <vlovev1011@gmail.com>
2016-11-10 20:44:50 -0800

    replace strart_screen

--- sjh/header/start_screen.h
+++ sjh/header/start_screen.h
@@ -2,4 +2,13 @@
 
 #include <stdio.h>
 
-int start_screen();
+int start_screen()
+{
+        int speed;
+        printf("************Tetris*************\n");
+        printf("Speed Level (1~10) : ");
+        scanf("%d\n",&speed);
+        return speed;
+}
+
+
