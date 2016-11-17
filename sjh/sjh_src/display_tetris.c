e9e0d74618d7ffb15ee7a3a447770fdd86c5277a
ShinJaeHo <vlovev1011@gmail.com>
2016-11-16 16:47:34 -0800

    test display_tetris.c_1

--- sjh/sjh_src/display_tetris.c
+++ sjh/sjh_src/display_tetris.c
@@ -5,34 +5,23 @@
 
 void display_tetris(void)
 {
-	char tetris_block_table[21][10];
 
-	for(int i=0; i<21; i++)
+
+	for(int i=0; i<20; i++)
         {
-                for(int j=0; j<10; j++)
+                for(int j=1; j<9; j++)
                 {
-                        tetris_block_table[i][j]=tetris_table[i][j];
+                        tetris_table[i][j]='#';
                 }
-                printf("\n");
         }
 
 
-
-	for(int i=1; i<20; i++)
-	{
-		for(int j=1; j<9; j++)
-		{
-			tetris_block_table[i][j]='#';
-			printf("%c",tetris_block_table[i][j]);
-		}
-		printf("\n");
-	}
 
         for(int i=0; i<21; i++)
         {
                 for(int j=0; j<10; j++)
                 {
-                        printf("%c",tetris_block_table[i][j]);
+                        printf("%c",tetris_table[i][j]);
                 }
                 printf("\n");
         }
