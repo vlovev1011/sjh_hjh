09856d06ca5227da033c717faed37fd10c7f2ef2
Jaehyuk Hur <gjwogur71@naver.com>
2016-11-30 06:04:23 -0800

    drop

--- hjh/hjh_header/variable.h
+++ hjh/hjh_header/variable.h
@@ -65,5 +65,7 @@ extern long point;
 
 int refresh(int signum);
 int collision(int command);
-void block_init();
-
+void block_init(void);
+int game_start(void);
+int move_block(int command);
+int drop(void);
---
+++ hjh/hjh_src/drop.c
@@ -0,0 +1,9 @@
+#include "variable.h"
+
+int drop(void)
+{
+	while(!collision(DOWN))
+		move_block(DOWN);
+
+	return;
+}
